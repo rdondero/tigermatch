18f2cc7f0fe0852585db6f1608aeb5f1ef3c1860
Kevin <kl29@princeton.edu>
2020-08-20 15:09:41 -0500

    person model

--- tm/person/models.py
+++ tm/person/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
 class Person(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    pic = models.CharField(max_length=500)
+    # cloudinary img link (in Postgres DB)
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    filter_gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    major = models.CharField(max_length=30)
+    grad_year = models.IntegerField()
+    settle_location = models.CharField(max_length=30)
+    career = models.CharField(max_length=30)
+    bio = models.CharField(max_length=500)
+
