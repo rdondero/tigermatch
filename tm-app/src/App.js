6429acd1884218d4b766232b71e371b67808ae59
Kevin Lin <kl29@princeton.edu>
2020-10-05 12:29:09 -0500

    landing pg

--- tm-app/src/App.js
+++ tm-app/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import pton from './img/pton10.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={pton} alt="" width="900" height="500"></img>
         <h1>Welcome to TIGERMATCH!</h1>
         <p>
           Edit "My Profile" to get started!
         </p>
+        
       </header>
     </div>
   );
